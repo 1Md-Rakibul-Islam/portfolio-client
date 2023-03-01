d7b6f98c34d24b3ec0e52d253b292101d28c6a9f
1Md-Rakibul-Islam <rakibul.islam199tech@gmail.com>
2023-02-28 22:37:40 -0800

    remove  daisy ui and added castom navbar

---
+++ src/Hooks/useScrollPosition.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const useScrollPosition = () => {
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useScrollPosition;
--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -14,28 +14,44 @@ const NavBar = () => {
     </React.Fragment>
 
   return (
-    <div className="navbar justify-between bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
-          </label>
-          <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            {menuItems}
-          </ul>
-        </div>
-        <Link to='/' className=" bg-purple-100 rounded-b-full border-spacing-2 border-purple-700">
-          <img className="w-[50px]" src={brandLogo} alt="" srcset="" />
-        </Link>
+    <nav className="relative bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+      <a href="https://flowbite.com/" className="flex items-center">
+          <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      </a>
+      <div className="flex items-center md:order-2">
+          <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+            <span className="sr-only">Open user menu</span>
+            <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo" />
+          </button>
+
+          <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+            <div className="px-4 py-3">
+              <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+              <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+            </div>
+            <ul className="py-2" aria-labelledby="user-menu-button">
+              {
+                menuItems
+              }
+            </ul>
+          </div>
+          <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="flex gap-8 p-0">
-            {menuItems}
+      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+        <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          {
+            menuItems
+          }
         </ul>
       </div>
-    </div>
+      </div>
+    </nav>
+
   );
 };
 
