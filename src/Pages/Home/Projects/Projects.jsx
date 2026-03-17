3966af2e19886ff4147aa6dbc82f1fadcb058323
1Md-Rakibul-Islam <rakibul.islam.developer@gmail.com>
2026-03-18 04:12:58 +0600

    feat: project section updated

--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -2,9 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import CardLoading from "../../../Components/Loadings/CardLoading/CardLoading";
 import Project from "./Project";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Projects = () => {
+  const { pathname } = useLocation();
+
   const {
     data: projects,
     isLoading,
@@ -35,16 +37,18 @@ const Projects = () => {
             <Project key={idx} project={project}></Project>
           ))}
         </div>
-        <div className="flex justify-center">
-          <Link
-            to={"/portfolio"}
-            className="relative mt-5 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-full group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-purple-800"
-          >
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-full group-hover:bg-opacity-0">
-              See More
-            </span>
-          </Link>
-        </div>
+        {pathname !== "/portfolio" && (
+          <div className="flex justify-center">
+            <Link
+              to={"/portfolio"}
+              className="relative mt-5 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium rounded-full group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-purple-800"
+            >
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-full group-hover:bg-opacity-0">
+                See More
+              </span>
+            </Link>
+          </div>
+        )}
       </div>
     </section>
   );
